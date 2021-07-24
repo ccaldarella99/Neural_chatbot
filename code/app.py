b632ac45d7a105dbe07d1b94aaafce22471fad81
ccaldarella99 <christopher.caldarella@outlook.com>
2021-07-23 22:32:38 -0400

    Created Flask app, moved tk-ver to own file

--- code/app.py
+++ code/app.py
@@ -1,5 +1,6 @@
 # This script runs the chat bot in a tkinter GUI
 
+import datetime
 import json
 import random
 import pickle
@@ -10,23 +11,19 @@ import nltk
 # nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
 
-import time
-import tkinter as tk
-
-# GLOBAL VARS
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#17202A"
-TEXT_COLOR = "#EAECEE"
-FONT = "Helvetica 14"
-FONT_BOLD = "Helvetica 13 bold"
-bot_name = 'Sam'
-
+# import time
+from flask import Flask, render_template  # , Response
+from flask_socketio import SocketIO
+# from flask.ext.socketio import SocketIO, emit
 
 # Files and Paths
 intents_file_path = '../data/intents.json'
 all_data_pickle_file_path = '../models/all_data.pkl'
 chatbot_model_file_path = '../models/chatbotmodel.h5'
 
+
+def log_dt(log_level = 'INFO'):
+    return datetime.datetime.now().isoformat() + ' [' + log_level.upper() + '] : '
 
 
 class ChatBot:
@@ -86,84 +83,69 @@ class ChatBot:
         return result
 
 
-class TkWindow:
-    
-    def __init__(self, chat_bot):
-        self.window = tk.Tk()
-        self._setup_main_window()
-        self.chat_bot = chat_bot
-    
-    def run(self):
-        self.window.mainloop()
+class FlaskWindow:
 
-    def _setup_main_window(self):
-        self.window.title("Chat")
-        self.window.resizable(width=False, height=False)
-        self.window.configure(width=470, height=550, bg=BG_COLOR)
+    def __init__(self):
+        self.convo = []
 
-        # head label
-        head_label = tk.Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
-
-        # tiny divider
-        line = tk.Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
-
-        # text widget
-        self.text_widget = tk.Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
-        self.text_widget.configure(cursor="arrow", state=tk.DISABLED)
-
-        # scroll bar
-        scrollbar = tk.Scrollbar(self.text_widget)
-        scrollbar.place(relheight=1, relx=0.984)
-        scrollbar.configure(command=self.text_widget.yview)
-
-        # bottom label
-        bottom_label = tk.Label(self.window, bg=BG_GRAY, height=80)
-        bottom_label.place(relwidth=1, rely=0.825)
-
-        # message entry box
-        self.msg_entry = tk.Entry(
-            bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
-        self.msg_entry.place(relwidth=0.74, relheight=0.06,
-                             rely=0.008, relx=0.011)
-        self.msg_entry.focus()
-        self.msg_entry.bind("<Return>", self._on_enter_pressed)
-
-        # send button
-        send_button = tk.Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY, command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
-    def _on_enter_pressed(self, event):  # event will take the argument entry
-        msg = self.msg_entry.get()       # get input text as string
-        self._insert_message(msg, "You")
-
-    def _insert_message(self, msg, sender):
-        if not msg:
+    def reply_message(self, msg, sender):
+        if(not msg):
             return
-
-        self.msg_entry.delete(0, tk.END)  # delete msg from txt_entry window
-        msg1 = f"{sender}: {msg}\n\n"     # The users message
-        self.text_widget.configure(state=tk.NORMAL)
-        self.text_widget.insert(tk.END, msg1)
-        self.text_widget.configure(state=tk.DISABLED)
-
-        # ints = self.chat_bot.predict_class(msg)
-        res = self.chat_bot.get_response(msg)
-        # time.sleep(1)
-        time.sleep(0.1)
-        msg2 = f"{self.chat_bot.bot_name}: {res}\n\n"
-        self.text_widget.configure(state=tk.NORMAL)
-        self.text_widget.insert(tk.END, msg2)
-        self.text_widget.configure(state=tk.DISABLED)
-
-        self.text_widget.see(tk.END)
+        self.convo.append(f'{sender}: {msg}\n\n')
 
 
-if __name__ == "__main__":
+app = Flask(__name__)
+# change this and make it pull from a file so it does not pull to github
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@app.route('/', methods=['POST'])
+def sessions():
+    # bot_name = 'Sam'
+    # cb = ChatBot(bot_name, intents_file_path,
+    #              all_data_pickle_file_path, chatbot_model_file_path)
+    # form = FlaskWindow()
+
+    # you_msg = {read in name=you from index.html}
+    # form.reply_message(you_msg, "You")
+    # form.reply_message(cb.get_response(you_msg), cb.bot_name)
+    
+    # Probably not use:
+    # chat_responses = []
+    # for i in form.convo:
+    #     chat_responses.append(i)
+    #     print(i)
+
+    return render_template('index.html') #, form=form, chat_responses=chat_responses)
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was recieved.')
+
+
+def messageSent(methods=['GET', 'POST']):
+    print('message was sent.')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json_msg, methods=['GET', 'POST']):
+    print(f'{log_dt()} RCVD: {str(json_msg)}')
+    socketio.emit('my response', json_msg, callback=messageReceived)
+    
+    print(f'\n\nTRY EXTRACT MESSAGE FROM: {json_msg}')
+    get_msg = json_msg['message']
+    send_msg = cb.get_response(get_msg)
+    print(f'TRY EXTRACT MESSAGE : {send_msg}\n')
+    mock_data = {'user_name': cb.bot_name.upper(), 'message': send_msg}
+
+    sam_json = json.dumps(mock_data, sort_keys=True)
+    socketio.emit('sam response', sam_json, callback=messageSent)
+    print(f'{log_dt()} SENT: {str(sam_json)}')
+
+
+if(__name__ == '__main__'):
+    bot_name = 'Sam'
     cb = ChatBot(bot_name, intents_file_path,
-                  all_data_pickle_file_path, chatbot_model_file_path)
-    win = TkWindow(cb)
-    win.run()
-
+                 all_data_pickle_file_path, chatbot_model_file_path)
+    socketio.run(app, debug=True)
